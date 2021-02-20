9ce8cb23f4185e3d2a5ee00653ba41aee22faeb5
JeffShell <875899952@qq.com>
2021-02-20 17:06:30 +0800

    翻译API调用

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,5 +2,47 @@
 const app = getApp()
 
 Page({
-
+	data:{
+		query: '',
+		hideClearIcon: true,
+		result: [],
+		curLang: {}
+	},
+	onLoad: function(options){
+		console.log('lonload..')
+		console.log(options)
+		if(options.query){
+			this.setData({
+				query: options.query
+			})
+		}
+	},
+	onShow: function(){
+		if(this.data.curLang.lang !== app.globalData.curLang.lang){
+			this.setData({curLang: app.globalData.curLang})
+			this.onConfirm()
+		}
+	},
+	onInput: function(e){
+		this.setData({'query': e.detail.value})
+		if(this.data.query.length > 0){
+			this.setData({'hideClearIcon': false})
+		} else {
+			this.setData({'hideClearIcon': true})
+		}
+		console.log('focus')
+	},
+	onTapClose: function(){
+		this.setData({query: '', hideClearIcon: true})
+	},
+	onConfirm: function(){
+		if(!this.data.query) return
+		translate(this.data.query, {from: 'auto', to: this.data.curLang.lang}).then(res => {
+			this.setData({'result': res.trans_result})
+			let history = wx.getStorageSync('history') || []
+			history.unshift({query: this.data.query, result: res.trans_result[0].dst})
+			history.length = history.length > 10 ? 10 : history.length
+			wx.setStorageSync('history', history)
+		})
+	}
 })
