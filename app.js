7fb9f83938208d5fd6db86ed66365999f0ec8c75
JeffShell <875899952@qq.com>
2021-02-20 17:24:46 +0800

    默认语言

--- app.js
+++ app.js
@@ -1,4 +1,47 @@
 // app.js
 App({
-
+	onLaunch: function(){
+		this.globalData.curLang = wx.getStorageSync("curLang") || this.globalData.langList[0]
+	},
+	globalData: {
+		curLang: {},
+		langList: [
+			{
+				'chs': '英文',
+				'lang': 'en',
+				'index': 0
+			},
+			{
+				'chs': '中文',
+				'lang': 'zh',
+				'index': 1
+			},
+			{
+				'chs': '日语',
+				'lang': 'jp',
+				'index': 2
+			},
+			{
+				'chs': '韩语',
+				'lang': 'kor',
+				'index': 3
+			},
+			{
+				'chs': '法语',
+				'lang': 'fra',
+				'index': 4
+			},
+			{
+				'chs': '西班牙语',
+				'lang': 'spa',
+				'index': 5
+			},
+			{
+				'chs': '阿拉伯语',
+				'lang': 'ara',
+				'index': 6
+			},
+			
+		]
+	}
 })
